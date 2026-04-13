0cb7205ddb46f3e31a4e651f92dbc07493430147
Aslan Bezhanov <bezhanov@sfedu.ru>
2026-04-13 23:22:38 +0300

    add: docker files

--- app/main.py
+++ app/main.py
@@ -48,4 +48,4 @@ def get_item_info(product_id: int):
     return result
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8080)
