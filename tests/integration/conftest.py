6590ac2c792924040d29388abc5b171c373114dc
Aslan Bezhanov <bezhanov@sfedu.ru>
2026-04-16 13:17:27 +0300

    fix: linters

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -3,9 +3,11 @@ import os
 import pytest
 import dotenv
 
+
 @pytest.fixture(autouse=True, scope="session")
 def envs():
     dotenv.load_dotenv()
+
 
 @pytest.fixture(scope="session")
 def app_url():
--- tests/integration/test_wildberries_service.py
+++ tests/integration/test_wildberries_service.py
@@ -1,9 +1,8 @@
 import requests
-import pytest
-
 from http import HTTPStatus
 
 TEST_PRODUCT_ID = 122268164
+
 
 def test_get_correct_product_info(app_url):
     response = requests.get(f"{app_url}/api/wb/{TEST_PRODUCT_ID}")
@@ -12,11 +11,15 @@ def test_get_correct_product_info(app_url):
     body = response.json()
     assert body["nm_id"] == TEST_PRODUCT_ID, "Product have correct ID"
     assert isinstance(body["name"], str), "Product have name"
-    assert isinstance(body['basic_kop'], int), \
-        "Product have correct basic price (kop)"
-    assert isinstance(body['product_kop'], int), \
-        "Product have correct product price (kop)"
-    assert isinstance(body["basic_rub"], float), \
-        "Product have correct basic price (rub)"
-    assert isinstance(body["product_rub"], float), \
-        "Product have correct product price (rub)"
+    assert isinstance(
+        body["basic_kop"], int
+    ), "Product have correct basic price (kop)"
+    assert isinstance(
+        body["product_kop"], int
+    ), "Product have correct product price (kop)"
+    assert isinstance(
+        body["basic_rub"], float
+    ), "Product have correct basic price (rub)"
+    assert isinstance(
+        body["product_rub"], float
+    ), "Product have correct product price (rub)"
